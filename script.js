e5e6171a5913036986e254d5aa9bcf72f9f9fe8c
ahmad ossman <ahmadossman2024@gmail.com>
2024-11-09 05:33:52 -0800

    tic tac toe Game

---
+++ script.js
@@ -0,0 +1,72 @@
+﻿const cells = document.querySelectorAll('.cell');
+const messageDisplay = document.querySelector('.message');
+const restartButton = document.getElementById('restart');
+
+let currentPlayer = 'X';
+let gameActive = true;
+let boardState = ['', '', '', '', '', '', '', '', ''];
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+function handleCellClick(event) {21
+    const clickedCell = event.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-index'));
+
+    if (boardState[clickedCellIndex] !== '' || !gameActive) {
+        return;
+    }
+
+    boardState[clickedCellIndex] = currentPlayer;
+    clickedCell.textContent = currentPlayer;
+    checkResult();
+}
+
+function checkResult() {
+    let roundWon = false;
+
+    for (let i = 0; i < winningConditions.length; i++) {
+        const [a, b, c] = winningConditions[i];
+        if (boardState[a] === '' || boardState[b] === '' || boardState[c] === '') {
+            continue;
+        }
+        if (boardState[a] === boardState[b] && boardState[a] === boardState[c]) {
+            roundWon = true;
+            break;
+        }
+    }
+
+    if (roundWon) {
+        messageDisplay.textContent =` فاز ${ currentPlayer } لاعب!`;
+        gameActive = false;
+        return;
+    }
+
+    if (!boardState.includes('')) {
+        messageDisplay.textContent = 'اللعبة انتهت بالتعادل!';
+        gameActive = false;
+    }
+
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+}
+
+function handleRestart() {
+    gameActive = true;
+    currentPlayer = 'X';
+    boardState = ['', '', '', '', '', '', '', '', ''];
+    messageDisplay.textContent = '';
+    cells.forEach(cell => {
+        cell.textContent = '';
+    });
+}
+
+cells.forEach(cell => cell.addEventListener('click', handleCellClick));
+restartButton.addEventListener('click', handleRestart);
